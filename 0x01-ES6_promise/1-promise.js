8c453fc99655a52c4dcc04db3fd13d7df0210bc0
yazgiraldoa <edygiraldo.97@gmail.com>
2022-04-13 00:27:03 -0500

    add funct with promise handling errors

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise(((resolve, reject) => {
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
+    }
+    reject(new Error('The fake API is not working currently'));
+  }));
+  return promise;
+}
