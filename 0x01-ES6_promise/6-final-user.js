8030b24e6b7465a046020854484968e78e4edb8c
yazgiraldoa <edygiraldo.97@gmail.com>
2022-04-13 23:24:36 -0500

    add funct that handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+
+  return Promise.allSettled([promise1, promise2])
+    .then((results) => {
+      const resultsList = [];
+
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          resultsList.push(result);
+        } else {
+          resultsList.push({ status: result.status, value: `${result.reason}` });
+        }
+      });
+      return resultsList;
+    });
+}
