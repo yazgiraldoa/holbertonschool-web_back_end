f8c21487a80dfb80a26c6bcf15d11923297ba430
yazgiraldoa <edygiraldo.97@gmail.com>
2022-04-14 00:00:23 -0500

    add function that handles all promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const uploadPhotoPromise = uploadPhoto();
+  const createUserPromise = createUser();
+
+  return Promise.all([uploadPhotoPromise, createUserPromise])
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+    })
+    .catch(() => { console.log('Signup system offline'); });
+}
