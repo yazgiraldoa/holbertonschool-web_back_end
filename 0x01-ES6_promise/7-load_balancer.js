1602216768e87c52f69a16d4f7d3afc9c4b78a79
yazgiraldoa <edygiraldo.97@gmail.com>
2022-04-13 22:54:26 -0500

    add funct with return first resolved promised

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,5 @@
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.any([chinaDownload, USDownload])
+    .then((result) => `${result}`)
+    .catch(() => new Error());
+}
