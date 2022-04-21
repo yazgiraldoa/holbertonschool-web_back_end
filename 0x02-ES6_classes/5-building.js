7fb5337f60faf728e398a0a7551308553efa96e2
yazgiraldoa <edygiraldo.97@gmail.com>
2022-04-20 22:43:17 -0500

    fix abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (this.evacuationWarningMessage === undefined) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   static evacuationWarningMessage() {
