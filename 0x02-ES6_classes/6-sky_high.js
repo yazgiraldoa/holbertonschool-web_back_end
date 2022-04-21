bafd740eaf59d4e5487e257c5f0278a8617d9387
yazgiraldoa <edygiraldo.97@gmail.com>
2022-04-20 22:49:33 -0500

    add method and getter

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -5,4 +5,12 @@ export default class SkyHighBuilding extends Building {
     super(sqft);
     this._floors = floors;
   }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+
+  get floors() {
+    return this._floors;
+  }
 }
